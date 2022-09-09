38ca40cd3f71f2b3eeef92ae95d5ab6c42eca631
Manumarfo <owurakumanumarfo@gmail.com>
2022-09-09 07:51:37 -0700

    shell parts

--- main.c
+++ main.c
@@ -1,180 +1,44 @@
 #include "shell.h"
 
 /**
- *  * main - the main function
- *   *
- *    * Return: (Success) 0 always
- *     * ------- (Fail) we drop out the looser :)
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-	sh_t data;
-	int pl;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	_memset((void *)&data, 0, sizeof(data));
-	signal(SIGINT, signal_handler);
-	while (1)
-	{
-		index_cmd(&data);
-		if (read_line(&data) < 0)
-		{
-			if (isatty(STDIN_FILENO))
-				PRINT("\n");
-			break;
-		}
-		if (split_line(&data) < 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		pl = parse_line(&data);
-		if (pl == 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		if (pl < 0)
-		{
-			print_error(&data);
-			continue;
-		}
-		if (process_cmd(&data) < 0)
-		{
-			print_error(&data);
-			break;
-		}
-		free_data(&data);
-	}
-	free_data(&data);
-	exit(EXIT_SUCCESS);
-}
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-/**
- *  * read_line - read a line from the standard input
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int read_line(sh_t *data)
-{
-	char *csr_ptr, *end_ptr, c;
-	size_t size = BUFSIZE, read_st, length, new_size;
-
-	data->line = malloc(size * sizeof(char));
-	if (data->line == NULL)
-		return (FAIL);
-	if (isatty(STDIN_FILENO))
-		PRINT(PROMPT);
-	for (csr_ptr = data->line, end_ptr = data->line + size;;)
+	if (ac == 2)
 	{
-		read_st = read(STDIN_FILENO, &c, 1);
-		if (read_st == 0)
-			return (FAIL);
-		*csr_ptr++ = c;
-		if (c == '\n')
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			*csr_ptr = '\0';
-			return (SUCCESS);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		if (csr_ptr + 2 >= end_ptr)
-		{
-			new_size = size * 2;
-			length = csr_ptr - data->line;
-			data->line = _realloc(data->line, size * sizeof(char),
-					new_size * sizeof(char));
-			if (data->line == NULL)
-				return (FAIL);
-			size = new_size;
-			end_ptr = data->line + size;
-			csr_ptr = data->line + length;
-		}
+		info->readfd = fd;
 	}
-}
-#define DELIMITER " \n\t\r\a\v"
-/**
- *  * split_line - splits line to tokens
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int split_line(sh_t *data)
-{
-	char *token;
-	size_t size = TOKENSIZE, new_size, i = 0;
-
-	if (_strcmp(data->line, "\n") == 0)
-		return (FAIL);
-	data->args = malloc(size * sizeof(char *));
-	if (data->args == NULL)
-		return (FAIL);
-	token = strtok(data->line, DELIMITER);
-	if (token == NULL)
-		return (FAIL);
-	while (token)
-	{
-		data->args[i++] =  token;
-		if (i + 2 >= size)
-		{
-			new_size = size * 2;
-			data->args = _realloc(data->args, size * sizeof(char *),
-					new_size * sizeof(char *));
-			if (data->args == NULL)
-				return (FAIL);
-			size = new_size;
-		}
-		token = strtok(NULL, DELIMITER);
-	}
-	data->args[i] = NULL;
-	return (0);
-}
-#undef DELIMITER
-#define DELIMITER ":"
-/**
- *  * parse_line - parses arguments to valid command
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int parse_line(sh_t *data)
-{
-	if (is_path_form(data) > 0)
-		return (SUCCESS);
-	if (is_builtin(data) > 0)
-	{
-		if (handle_builtin(data) < 0)
-			return (FAIL);
-		return (NEUTRAL);
-	}
-	is_short_form(data);
-	return (SUCCESS);
-}
-#undef DELIMITER
-/**
- *  * process_cmd - process command and execute process
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int process_cmd(sh_t *data)
-{
-	pid_t pid;
-	int status;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
-		if (execve(data->cmd, data->args, environ) < 0)
-			data->error_msg = _strdup("not found\n");
-		return (FAIL);
-	}
-	else
-	{
-		waitpid(pid, &status, WUNTRACED);
-	}
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
--- shell.h
+++ shell.h
@@ -1,180 +1,235 @@
-#include "shell.h"
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
 
 /**
- *  * main - the main function
- *   *
- *    * Return: (Success) 0 always
- *     * ------- (Fail) we drop out the looser :)
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
  */
-int main(void)
+typedef struct liststr
 {
-	sh_t data;
-	int pl;
-
-	_memset((void *)&data, 0, sizeof(data));
-	signal(SIGINT, signal_handler);
-	while (1)
-	{
-		index_cmd(&data);
-		if (read_line(&data) < 0)
-		{
-			if (isatty(STDIN_FILENO))
-				PRINT("\n");
-			break;
-		}
-		if (split_line(&data) < 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		pl = parse_line(&data);
-		if (pl == 0)
-		{
-			free_data(&data);
-			continue;
-		}
-		if (pl < 0)
-		{
-			print_error(&data);
-			continue;
-		}
-		if (process_cmd(&data) < 0)
-		{
-			print_error(&data);
-			break;
-		}
-		free_data(&data);
-	}
-	free_data(&data);
-	exit(EXIT_SUCCESS);
-}
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
 
 /**
- *  * read_line - read a line from the standard input
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
+ *struct passinfo - contains pseudo-arguements to pass into a function,
+ *		allowing uniform prototype for function pointer struct
+ *@arg: a string generated from getline containing arguements
+ *@argv: an array of strings generated from arg
+ *@path: a string path for the current command
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
+ *@fname: the program filename
+ *@env: linked list local copy of environ
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
  */
-int read_line(sh_t *data)
+typedef struct passinfo
 {
-	char *csr_ptr, *end_ptr, c;
-	size_t size = BUFSIZE, read_st, length, new_size;
-
-	data->line = malloc(size * sizeof(char));
-	if (data->line == NULL)
-		return (FAIL);
-	if (isatty(STDIN_FILENO))
-		PRINT(PROMPT);
-	for (csr_ptr = data->line, end_ptr = data->line + size;;)
-	{
-		read_st = read(STDIN_FILENO, &c, 1);
-		if (read_st == 0)
-			return (FAIL);
-		*csr_ptr++ = c;
-		if (c == '\n')
-		{
-			*csr_ptr = '\0';
-			return (SUCCESS);
-		}
-		if (csr_ptr + 2 >= end_ptr)
-		{
-			new_size = size * 2;
-			length = csr_ptr - data->line;
-			data->line = _realloc(data->line, size * sizeof(char),
-					new_size * sizeof(char));
-			if (data->line == NULL)
-				return (FAIL);
-			size = new_size;
-			end_ptr = data->line + size;
-			csr_ptr = data->line + length;
-		}
-	}
-}
-#define DELIMITER " \n\t\r\a\v"
-/**
- *  * split_line - splits line to tokens
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int split_line(sh_t *data)
-{
-	char *token;
-	size_t size = TOKENSIZE, new_size, i = 0;
-
-	if (_strcmp(data->line, "\n") == 0)
-		return (FAIL);
-	data->args = malloc(size * sizeof(char *));
-	if (data->args == NULL)
-		return (FAIL);
-	token = strtok(data->line, DELIMITER);
-	if (token == NULL)
-		return (FAIL);
-	while (token)
-	{
-		data->args[i++] =  token;
-		if (i + 2 >= size)
-		{
-			new_size = size * 2;
-			data->args = _realloc(data->args, size * sizeof(char *),
-					new_size * sizeof(char *));
-			if (data->args == NULL)
-				return (FAIL);
-			size = new_size;
-		}
-		token = strtok(NULL, DELIMITER);
-	}
-	data->args[i] = NULL;
-	return (0);
-}
-#undef DELIMITER
-#define DELIMITER ":"
-/**
- *  * parse_line - parses arguments to valid command
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int parse_line(sh_t *data)
-{
-	if (is_path_form(data) > 0)
-		return (SUCCESS);
-	if (is_builtin(data) > 0)
-	{
-		if (handle_builtin(data) < 0)
-			return (FAIL);
-		return (NEUTRAL);
-	}
-	is_short_form(data);
-	return (SUCCESS);
-}
-#undef DELIMITER
-/**
- *  * process_cmd - process command and execute process
- *   * @data: a pointer to the struct of data
- *    *
- *     * Return: (Success) a positive number
- *      * ------- (Fail) a negative number
- */
-int process_cmd(sh_t *data)
-{
-	pid_t pid;
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
 	int status;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		signal(SIGINT, SIG_DFL);
-		if (execve(data->cmd, data->args, environ) < 0)
-			data->error_msg = _strdup("not found\n");
-		return (FAIL);
-	}
-	else
-	{
-		waitpid(pid, &status, WUNTRACED);
-	}
-	return (0);
-}
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+
+/* toem_shloop.c */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* toem_parser.c */
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/* loophsh.c */
+int loophsh(char **);
+
+/* toem_errors.c */
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+
+/* toem_string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* toem_string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* toem_exits.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/* toem_tokenizer.c */
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+
+/* toem_realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* toem_memory.c */
+int bfree(void **);
+
+/* toem_atoi.c */
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+/* toem_errors1.c */
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+
+/* toem_builtin.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* toem_builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+/*toem_getline.c */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+
+/* toem_getinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+
+/* toem_environ.c */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/* toem_getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/* toem_history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/* toem_lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* toem_lists1.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+/* toem_vars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
+
+#endif
